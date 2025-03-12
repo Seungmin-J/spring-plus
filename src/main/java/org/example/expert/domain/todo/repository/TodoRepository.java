55d4680bdd919280bc9a13224b80c9832bd79116
seungminJ <seungmin103@gmail.com>
2025-03-12 09:10:48 +0900

    레벨 1

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,4 +20,16 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    Page<Todo> findAllByWeather(String weather, Pageable pageable);
+
+
+    @Query("select t from Todo t join fetch t.user u " +
+            "where t.modifiedAt between :startDate and :endDate " +
+            "and (:weather is null or t.weather = :weather)")
+    Page<Todo> findAllByWeatherAndModifiedAt(Pageable pageable,
+                       @Param("weather") String weather,
+                       @Param("startDate") LocalDateTime startDate,
+                       @Param("endDate") LocalDateTime endDate);
+
 }
